ce790ee58917af6c2f9cda5afe34bc3f0240924a
Conichan <conichan139@gmail.com>
2025-08-18 00:29:37 -0300

    trabajos en carrusel de portfolio

---
+++ carrusel.js
@@ -0,0 +1,15 @@
+    //carrusel
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapper = document.querySelector('.portfolio-row-wrapper');
+    const row = wrapper.querySelector('.portfolio-row');
+    const prev = wrapper.querySelector('.prev');
+    const next = wrapper.querySelector('.next');
+
+    next.addEventListener('click', () => {
+        row.scrollBy({ left: 340, behavior: 'smooth' }); // ajusta según el ancho de la tarjeta + gap
+    });
+
+    prev.addEventListener('click', () => {
+        row.scrollBy({ left: -340, behavior: 'smooth' });
+    });
+});
